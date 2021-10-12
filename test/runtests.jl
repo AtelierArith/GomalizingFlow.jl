9301aefcc6ab1958dfd5ceb1a8fe9d3aeba8d6c9
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2021-10-13 00:27:41 +0900

    write test

---
+++ test/runtests.jl
@@ -0,0 +1,102 @@
+
+using LFT
+using TOML
+
+using Test
+
+function create_hp_example2d()
+    @info "load hyper parmeters"
+    device_id = 0
+    dp = LFT.DeviceParams(device_id)
+
+    batchsize = 64
+    epochs = 40
+    iterations = 100
+    base_lr = 0.001
+    opt = "ADAM"
+    prior = "Normal{Float32}(0.f0, 1.f0)"
+    tp = LFT.TrainingParams(; batchsize, epochs, iterations, base_lr, opt, prior)
+
+    L = 8
+    Nd = 2
+    M2 = -4.
+    lam = 8
+    pp = LFT.PhysicalParams(;L, Nd, M2, lam)
+    
+    n_layers = 16
+    hidden_sizes = [8, 8]
+    kernel_size = 3
+    inC = 1
+    outC = 2
+    use_final_tanh = true
+    mp = LFT.ModelParams(;n_layers, hidden_sizes, kernel_size, inC, outC, use_final_tanh)
+    
+    return LFT.HyperParams(dp, tp, pp, mp, "config.toml")
+end
+
+
+function create_hp_example3d()
+    @info "load hyper parmeters"
+    device_id = 1
+    dp = LFT.DeviceParams(device_id)
+
+    batchsize = 64
+    epochs = 300
+    iterations = 100
+    base_lr = 0.0015
+    opt = "ADAM"
+    prior = "Normal{Float32}(0.f0, 1.f0)"
+    tp = LFT.TrainingParams(; batchsize, epochs, iterations, base_lr, opt, prior)
+
+    L = 8
+    Nd = 3
+    M2 = -4.
+    lam = 8
+    pp = LFT.PhysicalParams(;L, Nd, M2, lam)
+    
+    n_layers = 16
+    hidden_sizes = [8, 8]
+    kernel_size = 3
+    inC = 1
+    outC = 2
+    use_final_tanh = true
+    mp = LFT.ModelParams(;n_layers, hidden_sizes, kernel_size, inC, outC, use_final_tanh)
+    
+    return LFT.HyperParams(dp, tp, pp, mp, "config.toml")
+end
+
+@testset "PhysicalParams property" begin
+    L = 8
+    Nd = 2
+    M2 = -4.
+    lam = 8
+    pp = LFT.PhysicalParams(;L, Nd, M2, lam)
+    @test pp.L == L
+    @test pp.lattice_shape == (L, L)
+    @test pp.m² == pp.M2
+    @test pp.λ == pp.lam
+end
+
+@testset "example2d.toml" begin
+    path = "../cfgs/example2d.toml"
+    ref_hp = create_hp_example2d()
+    tar_hp = LFT.load_hyperparams(path)
+    @test tar_hp.tp == ref_hp.tp
+    @test tar_hp.pp == ref_hp.pp
+    for f in fieldnames(typeof(ref_hp.mp))
+        @test getfield(tar_hp.mp, f) == getfield(ref_hp.mp, f)
+    end
+    #@test tar_hp.mp == ref_hp.mp # <--- fails... why?!
+end
+
+@testset "example3d.toml" begin
+    path = "../cfgs/example3d.toml"
+    ref_hp = create_hp_example3d()
+    tar_hp = LFT.load_hyperparams(path)
+    @test tar_hp.tp == ref_hp.tp
+    @test tar_hp.pp == ref_hp.pp
+    for f in fieldnames(typeof(ref_hp.mp))
+        @test getfield(tar_hp.mp, f) == getfield(ref_hp.mp, f)
+    end
+    #@test tar_hp.mp == ref_hp.mp <--- fails... why?!
+end
