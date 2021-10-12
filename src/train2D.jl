9e1e8268d47838be45d4efc22531e3e3f9b35116
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2021-10-12 17:59:01 +0900

    allow to save model

--- src/train2D.jl
+++ src/train2D.jl
@@ -3,12 +3,13 @@ using Parameters
 using Distributions
 using ProgressMeter
 using Flux
+using BSON
 
 using LFT
 using LFT:ScalarPhi4Action
 
 
-function load_hyperparams()
+function load_hyperparams2D()
     device_id = 0
     dp = LFT.DeviceParams(device_id)
 
@@ -37,8 +38,37 @@ function load_hyperparams()
     return LFT.HyperParams(dp, tp, pp, mp)
 end
 
+function load_hyperparams3D()
+    device_id = 1
+    dp = LFT.DeviceParams(device_id)
+
+    batchsize = 64
+    epochs = 300
+    iterations = 100
+    base_lr = 0.0015f0
+    opt = "ADAM"
+    prior = Normal{Float32}(0.f0, 1.f0)
+    tp = LFT.TrainingParams(; batchsize, epochs, iterations, base_lr, opt, prior)
+
+    L = 8
+    Nd = 3
+    m² = -4.
+    λ = 8
+    pp = LFT.PhysicalParams(;L, Nd, m², λ)
+    
+    n_layers = 16
+    hidden_sizes = (8, 8)
+    kernel_size = 3
+    inC = 1
+    outC = 2
+    use_final_tanh = true
+    mp = LFT.ModelParams(;n_layers, hidden_sizes, kernel_size, inC, outC, use_final_tanh)
+    
+    return LFT.HyperParams(dp, tp, pp, mp)
+end
+
 function train()
-    hp = load_hyperparams()
+    hp = load_hyperparams2D()
     device = hp.dp.device
     @info "setup action"
     phi4_action = ScalarPhi4Action(hp.pp.m², hp.pp.λ)
@@ -54,7 +84,7 @@ function train()
     @info "setup optimiser"
     opt = eval(Meta.parse("$(hp.tp.opt)($(hp.tp.base_lr))"))
     @info opt
-    
+
     @info "epoch loop"
     for _ in 1:epochs
         @showprogress for _ in 1:iterations
@@ -85,9 +115,11 @@ function train()
         logp = -phi4_action(x)
         loss = LFT.calc_dkl(logp, logq)
         @show loss
-        @show "loss per site" loss / prod(lattice_shape)
+        println("loss per site", loss / prod(lattice_shape))
         ess = LFT.compute_ess(logp, logq)
         @show ess
     end
-    layer
+
+    trained_layer = layer |> cpu
+    BSON.@save "layer.bson" trained_layer
 end
