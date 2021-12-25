c4e60ba1d0231a0c6fde14849a2b8f2be0c16ea7
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2021-12-25 18:30:09 +0900

    impl file watcher

---
+++ src/Watcher.jl
@@ -0,0 +1,32 @@
+module Watcher
+
+using FileWatching
+using UnicodePlots
+using CSV, DataFrames
+
+export serve
+
+function serve(dir, item::Symbol, title = "Evaluation")
+    file = joinpath(dir, "evaluations.csv")
+    while true
+        FileWatching.watch_file(file)
+        sleep(0.5)
+        @info "$(basename(file)) is updated"
+        try
+            df = CSV.read(file, DataFrame)
+            p = lineplot(
+                df.epoch, getproperty(df, item);
+                name = string(item),
+                title = title,
+                xlabel = "epoch",
+                ylabel = string(item),
+                width = 60
+            )
+            display(p)
+        catch
+            @warn "something happened"
+        end
+    end
+end
+
+end
