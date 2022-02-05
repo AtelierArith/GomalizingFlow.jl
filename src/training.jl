d6af186f6908df4f548e38c8cab4b284e7c65292
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2022-02-05 12:22:46 +0900

    run mcmc every epoch

--- src/training.jl
+++ src/training.jl
@@ -40,6 +40,7 @@ function train(hp)
         :ess => T[],
         :best_ess => T[],
         :best_epoch => Int[],
+        :accepted_ratio => T[],
     )
 
     @info "start training"
@@ -83,6 +84,18 @@ function train(hp)
         ess = compute_ess(logp, logq)
         @show ess
 
+        nsamples = 8196
+        history_current_epoch = make_mcmc_ensamble(
+            model |> cpu,
+            prior,
+            action,
+            lattice_shape;
+            batchsize,
+            nsamples,
+            device=cpu,
+        )
+        accepted_ratio = 100mean(history_current_epoch.accepted)
+        @show accepted_ratio
         # save best checkpoint
         if ess >= best_ess
             @info "Found best ess"
@@ -94,17 +107,17 @@ function train(hp)
             LFT.BSON.@save joinpath(result_dir, "trained_model_best_ess.bson") trained_model_best_ess
             # save mcmc history
             @info "make mcmc ensamble"
-            nsamples = 8196
-            history_best_ess = make_mcmc_ensamble(trained_model_best_ess, prior, action, lattice_shape; batchsize, nsamples, device = cpu)
+            history_best_ess = history_current_epoch
             @info "save history_best_ess to $(joinpath(result_dir, "history_best_ess.bson"))"
             LFT.BSON.@save joinpath(result_dir, "history_best_ess.bson") history_best_ess
-            Printf.@printf "accepted_ratio= %.2f [percent]" 100mean(history_best_ess.accepted)
+            Printf.@printf "accepted_ratio= %.2f [percent]" 100mean(
+                history_best_ess.accepted,
+            )
         end
 
-        push!(evaluations, Dict(pairs((; epoch, loss, ess, best_epoch, best_ess))))
+        push!(evaluations, Dict(pairs((; epoch, loss, ess, best_epoch, best_ess, accepted_ratio))))
 
         CSV.write(joinpath(result_dir, "evaluations.csv"), evaluations)
-
     end
     @info "finished training"
     trained_model = model |> cpu
