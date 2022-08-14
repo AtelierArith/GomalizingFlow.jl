1e1f143a6da2411241eb2594a0126d1d53a834d4
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2022-08-14 15:35:02 +0900

    impl rrule

--- src/actions.jl
+++ src/actions.jl
@@ -1,3 +1,5 @@
+using ChainRulesCore
+
 mutable struct ScalarPhi4Action{T<:AbstractFloat}
     m²::T
     λ::T
@@ -8,7 +10,7 @@ function calc_action(
     cfgs::AbstractArray{T,N},
 ) where {T<:AbstractFloat,N}
     cfgs² = cfgs .^ 2
-    potential = @. action.m² * cfgs² + action.λ * cfgs² ^ 2
+    potential = @. action.m² * cfgs² + action.λ * cfgs²^2
     sz = ndims(cfgs)
     Nd = sz - 1 # exclude last axis
     B = size(cfgs, sz) # batch size
@@ -18,7 +20,7 @@ function calc_action(
     action_density = @. potential + k1 - k2 - k3
     reshape(
         sum(action_density, dims=1:Nd),
-        B
+        B,
     )
 end
 
@@ -26,4 +28,52 @@ function (action::ScalarPhi4Action{T})(
     cfgs::AbstractArray{T,N},
 ) where {T<:AbstractFloat,N}
     calc_action(action, cfgs)
+end
+
+function ∂kinetic(cfgs::AbstractArray{T,N}) where {T,N}
+    F = zero(cfgs)
+    sz = ndims(cfgs)
+    Nd = sz - 1 # exclude last axis
+    dest = similar(cfgs)
+    F .+= Nd .* 4cfgs
+    for μ in 1:Nd
+        circshift!(dest, cfgs, -Flux.onehot(μ, 1:sz))
+        F .-= 2 .* dest
+    end
+    for μ in 1:Nd
+        circshift!(dest, cfgs, Flux.onehot(μ, 1:sz))
+        F .-= 2 .* dest
+    end
+    return F
+end
+
+function ∂potential(
+    action::ScalarPhi4Action{T},
+    cfgs::AbstractArray{T,N},
+) where {T,N}
+    @. 2action.m² * cfgs + 4 * action.λ * cfgs^3
+end
+
+function ∂kinetic(
+    ::ScalarPhi4Action{T},
+    cfgs::AbstractArray{T,N},
+) where {T,N}
+    ∂kinetic(cfgs)
+end
+
+function ChainRulesCore.rrule(
+    a::ScalarPhi4Action{T},
+    x::AbstractArray{T,N},
+) where {T,N}
+    y = a(x)
+    function pullback(ȳ)
+        sz = ndims(x)
+        B = size(x, sz)
+        Nd = sz - 1
+        ∂a∂x = ∂potential(a, x) .+ ∂kinetic(a, x)
+        # do multiplication for each batch
+        x̄ = ∂a∂x .* reshape(ȳ, ones(Int, Nd)..., B)
+        return NoTangent(), x̄
+    end
+    return y, pullback
 end
